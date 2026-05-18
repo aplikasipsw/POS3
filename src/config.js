1f067e2c878fe544eb8d6376d0ce154316f672ed
aplikasipsw <aplikasipsw@gmail.com>
2026-05-18 22:01:01 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // Central configuration for Nasi Goreng Premium POS system
 
-export const API_URL = "https://script.google.com/macros/s/AKfycbyU1l4Oa5hKnYv61MivYYvqpxK5jfVufsNIiWtPU5njzTF1TDIGtV-96A-lHa1aDDwdng/exec";
+export const API_URL = "https://script.google.com/macros/s/AKfycbxAWqnmQ2FQKloA8Xilz5Rvh-egZ96uuLmwIh0p5L_3GqFbH5NTmQ9wEM7S5KueCl5a1w/exec";
 
 export const APP_NAME = "Nasi Goreng Premium";
 export const DEFAULT_TAX_RATE = 0.11; // 11% PPN
